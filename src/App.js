f563db670f0fb0844af09d4c028fd22148be1398
Spencer Dixon <spencercdixon@gmail.com>
2015-10-21 11:47:47 -0400

    Initial commit.  Got elemental working

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { NICE, SUPER_NICE } from './colors';
+import { Alert } from 'elemental';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  tick() {
+    this.setState({
+      counter: this.state.counter + this.props.increment
+    });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <h1 style={{ color: this.props.color }}>
+        Counter ({this.props.increment}): {this.state.counter}
+      </h1>
+    );
+  }
+}
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Alert type="info"><strong>Info:</strong> This could be something helpful, better read it just to make sure.</Alert>
+        <Counter increment={1} color={NICE} />
+        <Counter increment={5} color={SUPER_NICE} />
+      </div>
+    );
+  }
+}
